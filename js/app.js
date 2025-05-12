59fbf6f08913e03b94b18a0c976ec7cf7939047d
Alhawari80 <anahawari80@gmail.com>
2025-05-12 19:56:33 +0300

    javascript-browser-game-tic-tac-toe

--- js/app.js
+++ js/app.js
@@ -27,54 +27,98 @@ let tie = false;
 
 const squareEls = document.querySelectorAll('.sqr');
 const messageEl = document.querySelector('#message');
+const resetButtonEl = document.querySelector('#reset');
 
 // Winning combinations
 const winningCombos = [
-    [0, 1, 2], 
-    [3, 4, 5], 
-    [6, 7, 8], 
-    [0, 3, 6], 
-    [1, 4, 7], 
-    [2, 5, 8], 
-    [0, 4, 8], 
-    [2, 4, 6] 
-  ];
-  
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
-
+squareEls.forEach(square => square.addEventListener('click', handleClick));
+resetButtonEl.addEventListener('click', init);
 
 /*-------------------------------- Functions --------------------------------*/
 
 // Initialize the game
 function init() {
-  board = ["", "", "", "", "", "", "", "", ""];
-  turn = "X";
-  winner = false;
-  tie = false;
-  render();
+    board = ["", "", "", "", "", "", "", "", ""];
+    turn = "X";
+    winner = false;
+    tie = false;
+    render();
 }
 // step 4 Render the game state and update board
 function render() {
-  updateBoard();
-  updateMessage();
+    updateBoard();
+    updateMessage();
 }
 // update board
-function updateBoard() {
-  board.forEach((sqr, i) => {
-    squareEls[i].innerText = sqr;
-  });
+function updateBoard() {board.forEach((sqr, i) => {
+squareEls[i].innerText = sqr;});
 }
 // update message
+function updateMessage() {
+  if (!winner && !tie) {
+    messageEl.innerText = `It's ${turn}'s turn`;
+  } else if (!winner && tie) {
+    messageEl.innerText = "It's a tie!";
+  } else {
+    messageEl.innerText = `Congrats, ${turn} wins!`;
+  }
+}
+
+// Handle square click
+function handleClick(event) {
+  const squareIndex = Array.from(squareEls).indexOf(event.target); //asked a friend
+  if (board[squareIndex] !== "" || winner) return;
+  placePiece(squareIndex);
+  checkForWinner();
+  checkForTie();
+  switchPlayerTurn();
+  render();
+}
+
 
 
 // step 5 Check for a winner
+function checkForWinner() {
+  winner = winningCombos.some(pattern => pattern.every(i => board[i] === turn)
+  );
+//   if (winner) {
+    // console.log('winner:' , '${turn}');
+  }
 
 
 // step 6
 // Place a piece on the board
+function placePiece(index) {
+    board[index] = turn;
+    // console.log('Board state:', board);
+  }
+//  Check for a tie
+function checkForTie() {
+  if (winner) return;
+  tie = !board.includes("");
+//   console.log('Tie:', tie);
+}
+
+function switchPlayerTurn() {
+  if (winner) return;
+  turn = turn === "X" ? "O" : "X";
+//   console.log('Turn:', turn);
+}
+
+init();
 
 
 
